f4ce5a1f9284d2df36ca41f375118d93cf3828ad
anviaan <andre.carbajal.vargas@gmail.com>
2022-01-19 21:41:25 -0500

    log block modified

--- src/main/java/net/anvian/simplemango/block/ModBlocks.java
+++ src/main/java/net/anvian/simplemango/block/ModBlocks.java
@@ -4,13 +4,10 @@ import net.anvian.simplemango.MangoMod;
 import net.anvian.simplemango.item.MangoItemGroup;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.Material;
-import net.minecraft.block.PillarBlock;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -21,16 +18,20 @@ public class ModBlocks {
             new Block(FabricBlockSettings.of(Material.WOOD).strength(0.5f).requiresTool().breakByHand(true)));
 
     public static final Block MANGO_LOG = registerBlock("mango_log",
-            new PillarBlock(FabricBlockSettings.copyOf(Blocks.OAK_LOG).requiresTool()));
+            new PillarBlock(FabricBlockSettings.of(Material.WOOD, MapColor.OAK_TAN)
+                    .strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).requiresTool()));
 
     public static final Block MANGO_WOOD = registerBlock("mango_wood",
-            new PillarBlock(FabricBlockSettings.copyOf(Blocks.OAK_WOOD).requiresTool().breakByHand(true)));
+            new PillarBlock(FabricBlockSettings.of(Material.WOOD, MapColor.OAK_TAN)
+                    .strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).requiresTool()));
 
     public static final Block STRIPPED_MANGO_LOG = registerBlock("stripped_mango_log",
-            new PillarBlock(FabricBlockSettings.copyOf(Blocks.STRIPPED_OAK_LOG).requiresTool().breakByHand(true)));
+            new PillarBlock(FabricBlockSettings.of(Material.WOOD, MapColor.OAK_TAN)
+                    .strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).requiresTool()));
 
     public static final Block STRIPPED_MANGO_WOOD = registerBlock("stripped_mango_wood",
-            new PillarBlock(FabricBlockSettings.copyOf(Blocks.STRIPPED_OAK_WOOD).requiresTool().breakByHand(true)));
+            new PillarBlock(FabricBlockSettings.of(Material.WOOD, MapColor.OAK_TAN)
+                    .strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).requiresTool()));
 
     public static final Block MANGO_PLANKS = registerBlock("mango_planks",
             new Block(FabricBlockSettings.copyOf(Blocks.OAK_PLANKS).requiresTool()));
