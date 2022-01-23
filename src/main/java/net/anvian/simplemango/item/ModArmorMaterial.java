fd72ca16674bbd95a19ac8da6a6d9a4970ce6f7c
anviaan <andre.carbajal.vargas@gmail.com>
2022-01-23 00:10:02 -0500

    gold mango and enchant gold mango

--- src/main/java/net/anvian/simplemango/item/ModArmorMaterial.java
+++ src/main/java/net/anvian/simplemango/item/ModArmorMaterial.java
@@ -2,9 +2,6 @@ package net.anvian.simplemango.item;
 
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.ItemConvertible;
-import net.minecraft.item.Items;
-import net.minecraft.item.ToolMaterial;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
