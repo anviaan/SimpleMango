fcc676f9c0bb80e8fafe7ae3eab1a6cedaf46360
anvian <andre.carbajal.vargas@gmail.com>
2022-07-10 16:53:26 -0500

    start forge port

--- src/main/java/net/anvian/simplemango/block/custom/ModWoodButton.java
+++
@@ -1,9 +0,0 @@
-package net.anvian.simplemango.block.custom;
-
-import net.minecraft.block.WoodenButtonBlock;
-
-public class ModWoodButton extends WoodenButtonBlock {
-    public ModWoodButton(Settings settings) {
-        super(settings);
-    }
-}
--- src/main/java/net/anvian/simplemango/world/gen/ModWorldGen.java
+++
@@ -1,7 +0,0 @@
-package net.anvian.simplemango.world.gen;
-
-public class ModWorldGen {
-    public static void generateModWorldGen() {
-        ModTreeGeneration.generateTrees();
-    }
-}
