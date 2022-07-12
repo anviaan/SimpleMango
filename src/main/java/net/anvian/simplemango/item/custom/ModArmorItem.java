a637d63738d4280ee9f6d38d7bbd579e83b2b6e6
anvian <andre.carbajal.vargas@gmail.com>
2022-07-12 14:52:09 -0500

    modify effect amplifier

--- src/main/java/net/anvian/simplemango/item/custom/ModArmorItem.java
+++ src/main/java/net/anvian/simplemango/item/custom/ModArmorItem.java
@@ -19,7 +19,7 @@ public class ModArmorItem extends ArmorItem {
     private static final Map<ArmorMaterial, MobEffectInstance> MATERIAL_TO_EFFECT_MAP =
             (new ImmutableMap.Builder<ArmorMaterial, MobEffectInstance>())
                     .put(ModArmorMaterial.SEED,
-                            new MobEffectInstance(MobEffects.LUCK, 200, 1)).build();
+                            new MobEffectInstance(MobEffects.LUCK, 200, 0)).build();
 
     public ModArmorItem(ArmorMaterial material, EquipmentSlot slot, Properties settings) {
         super(material, slot, settings);
