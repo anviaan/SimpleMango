10a7e629179ea17175402f704bf2c922de0ab42f
anviaan <andre.carbajal.vargas@gmail.com>
2022-11-16 14:21:14 -0500

    unnecessary classes deleted

--- src/main/java/net/anvian/simplemango/block/custom/ModTrapdoorBlock.java
+++
@@ -1,9 +0,0 @@
-package net.anvian.simplemango.block.custom;
-
-import net.minecraft.block.TrapdoorBlock;
-
-public class ModTrapdoorBlock extends TrapdoorBlock {
-    public ModTrapdoorBlock(Settings settings) {
-        super(settings);
-    }
-}
