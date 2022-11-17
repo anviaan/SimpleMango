0bfdce01cb09968485a08dd242c0c1e06eb5664d
anviaan <andre.carbajal.vargas@gmail.com>
2022-11-17 16:48:23 -0500

    FIX

---
+++ src/main/java/net/anvian/simplemango/util/ModComposting.java
@@ -0,0 +1,11 @@
+package net.anvian.simplemango.util;
+
+import net.anvian.simplemango.item.ModItems;
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
+
+public class ModComposting {
+    public static void register(){
+        CompostingChanceRegistry.INSTANCE.add(ModItems.SEED,0.25f);
+        CompostingChanceRegistry.INSTANCE.add(ModItems.MANGO, 0.65f);
+    }
+}
