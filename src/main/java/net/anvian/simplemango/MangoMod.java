af3b41845a38a9341cb2d382112904b072c2fffc
Andre Carbajal <andre.carbajal.vargas@gmail.com>
2023-01-17 21:28:16 -0500

    MangoMod

--- src/main/java/net/anvian/simplemango/MangoMod.java
+++ src/main/java/net/anvian/simplemango/MangoMod.java
@@ -1,26 +1,26 @@
 package net.anvian.simplemango;
 
 import net.anvian.simplemango.block.ModBlocks;
+import net.anvian.simplemango.item.MangoItemGroup;
 import net.anvian.simplemango.item.ModItems;
 import net.anvian.simplemango.registries.ModRegistries;
 import net.anvian.simplemango.util.ModComposting;
 import net.anvian.simplemango.util.ModFlammableBlock;
 import net.anvian.simplemango.util.ModLootTableModifiers;
-import net.anvian.simplemango.world.features.ModConfiguredFeatures;
 import net.anvian.simplemango.world.gen.ModWorldGen;
 import net.fabricmc.api.ModInitializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MangoMod implements ModInitializer {
-	public static final String MOD_ID = "simplemango";
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public 	static final String MOD_ID = "simplemango";
+	public static final Logger LOGGER = LoggerFactory.getLogger("simplemango");
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
 
-		ModConfiguredFeatures.registerConfiguredFeatures();
+		MangoItemGroup.registerItemGroup();
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
