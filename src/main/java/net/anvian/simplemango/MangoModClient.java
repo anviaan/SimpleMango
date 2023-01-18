fbcda76d716ee30b24b5be90087ac20e69b7269b
Andre Carbajal <andre.carbajal.vargas@gmail.com>
2023-01-17 22:42:17 -0500

    renderHelper

--- src/main/java/net/anvian/simplemango/MangoModClient.java
+++ src/main/java/net/anvian/simplemango/MangoModClient.java
@@ -1,11 +1,12 @@
 package net.anvian.simplemango;
 
+import net.anvian.simplemango.util.ModRenderHelper;
 import net.fabricmc.api.ClientModInitializer;
 
 public class MangoModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        //ModRenderHelper.setRenderLayers();
+        ModRenderHelper.setRenderLayers();
 
     }
 }
