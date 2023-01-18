9b8cea3707b5e9a5c457e670a7ff834d9613d728
Andre Carbajal <andre.carbajal.vargas@gmail.com>
2023-01-17 22:41:58 -0500

    renderHelper

--- src/main/java/net/anvian/simplemango/MangoModClient.java
+++ src/main/java/net/anvian/simplemango/MangoModClient.java
@@ -1,13 +1,11 @@
 package net.anvian.simplemango;
 
-import net.anvian.simplemango.util.ModRenderHelper;
 import net.fabricmc.api.ClientModInitializer;
 
 public class MangoModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
-        ModRenderHelper.setRenderLayers();
+        //ModRenderHelper.setRenderLayers();
 
     }
 }
--- src/main/java/net/anvian/simplemango/util/ModRenderHelper.java
+++ src/main/java/net/anvian/simplemango/util/ModRenderHelper.java
@@ -1,15 +1,14 @@
 package net.anvian.simplemango.util;
 
-import net.anvian.simplemango.block.ModBlocks;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.minecraft.client.render.RenderLayer;
+import net.anvian.simplemango.MangoMod;
 
 public class ModRenderHelper {
     public static void setRenderLayers(){
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MANGO_TRAPDOOR, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MANGO_DOOR, RenderLayer.getCutout());
-
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MANGO_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MANGO_LEAVES, RenderLayer.getCutout());
+//        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MANGO_TRAPDOOR, RenderLayer.getCutout());
+//        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MANGO_DOOR, RenderLayer.getCutout());
+//
+//        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MANGO_SAPLING, RenderLayer.getCutout());
+//        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MANGO_LEAVES, RenderLayer.getCutout());
+        MangoMod.LOGGER.info("I need to fix this");
     }
 }
